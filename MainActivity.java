2e73c5927716bf212ebddc8d3f9bf16a08cb2bd2
zhangs <zhangsgm@gmail.com>
2017-03-08 19:39:19 -0500

    test

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.zhangs.mychess17;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //
+    }
+}
